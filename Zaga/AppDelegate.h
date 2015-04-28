c2f0cf525fe26347090ce95aa5d955e902a8708b
Miguel Araújo <miguelarauj1o@gmail.com>
2015-04-27 21:10:37 -0300

    First

---
+++ Zaga/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zaga
+//
+//  Created by Miguel Araújo on 4/27/15.
+//  Copyright (c) 2015 Miguel Araújo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
